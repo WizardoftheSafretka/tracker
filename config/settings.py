1277c0e9ab92eca9cb87d3f60aff965eed479cc5
Wizard <merihov2yandex.ru>
2026-07-14 21:09:08 +0300

    task django rest

--- config/settings.py
+++ config/settings.py
@@ -102,9 +102,9 @@ USE_I18N = True
 USE_TZ = True
 
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "static"
 
 AUTH_USER_MODEL = "users.User"
 
