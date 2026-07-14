45aba8949c0a02e3853bccba0030184bff19e780
Wizard <merihov2yandex.ru>
2026-07-14 20:54:44 +0300

    task django rest

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,6 @@ import os
 from datetime import timedelta
 from pathlib import Path
 
-from django.conf.global_settings import STATICFILES_DIRS, STATIC_ROOT
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
