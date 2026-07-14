c0d5551cd6661c215ca673c60ab9378388d47e9e
Wizard <merihov2yandex.ru>
2026-07-14 21:19:04 +0300

    task django rest

--- config/settings.py
+++ config/settings.py
@@ -103,8 +103,11 @@ USE_TZ = True
 
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = []
 STATIC_ROOT = BASE_DIR / "static"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 AUTH_USER_MODEL = "users.User"
 
